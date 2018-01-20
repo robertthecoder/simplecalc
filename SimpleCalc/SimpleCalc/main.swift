1a0fcb41c1c8a16466fc3a8251b580019871f414
Robert J. Kim <robert7kim7@gmail.com>
2018-01-19 23:27:46 -0800

    first version

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -8,19 +8,114 @@
 
 import Foundation
 
-public class Calculator {
-    public func calculate(_ args: [String]) -> Int {
+open class Calculator {
+    open func calculate(_ args: [String]) -> Int {
+        var sum: Int = 0;
+        
+        if (args.indices.contains(1)) {
+            // Basic Calculations:
+            if (args[1] == "+") {
+                sum = Int(args[0])! + Int(args[2])!
+            } else if (args[1] == "-") {
+                sum = Int(args[0])! - Int(args[2])!
+            } else if (args[1] == "*") {
+                sum = Int(args[0])! * Int(args[2])!
+            } else if (args[1] == "/") {
+                sum = Int(args[0])! / Int(args[2])!
+            } else if (args[1] == "%") {
+                sum = Int(args[0])! % Int(args[2])!
+            }
+        }
+        
+        
+        
+        // Information about Input:
+        if (args[args.count - 1] == "count") {
+            return args.count - 1;
+        
+        } else if (args[args.count - 1] == "avg") {
+            var avg = 0;
+            
+            if (args.count >= 2) {
+                for index in 0...args.count - 2 {
+                    avg = avg + Int(args[index])!;
+                }
+            }
+            
+            if ((args.count - 1) > 0) {
+                avg = avg / (args.count - 1);
+            }
+            return avg;
+        } else if (args[args.count - 1] == "fact") {
+            var factorial = 1;
+            
+            if ((args.count - 1) > 1) {
+                for index in 1...Int(args.count - 1) {
+                    factorial = factorial * index;
+                }
+            }
+
+            return factorial;
+        }
+        
+        return sum;
     }
     
-    public func calculate(_ arg: String) -> Int {
+    open func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
     }
 }
 
 print("UW Calculator v1")
 print("Enter an expression separated by returns:")
+
 let first = readLine()!
-let operation = readLine()!
-let second = readLine()!
-print(Calculator().calculate([first, operation, second]))
+
+if (first == "avg" || first == "count" || first == "fact") {
+    print(0)
+} else {
+    let operation = readLine()!
+    
+    // If asking for avg on index 1
+    if (operation == "avg") {
+        print(first)
+//    } else if (operation == "fact") {
+//        var factorial = 1;
+//
+//        if (first == "0") {
+//            print(1)
+//        } else {
+//            for index in 1...Int(first)! {
+//                factorial = factorial * index;
+//            }
+//
+//            print(factorial)
+//        }
+       
+    } else {
+        let second = readLine()!
+        var array = [first, operation, second]
+        
+        // For the calculations:
+        if (operation == "+") || (operation == "-") || (operation == "*") || (operation == "/") || (operation == "%") {
+            print(Calculator().calculate(array))
+            // For getting information about the #'s
+        } else if (first == "avg") {
+            print("0")
+        } else {
+            var temp = readLine()!
+            array.append(temp);
+            
+            while (array[array.count - 1] != "count") && (array[array.count - 1] != "avg") && (array[array.count - 1]  != "fact") {
+                temp = readLine()!
+                array.append(temp);
+            }
+            
+            print(Calculator().calculate(array))
+        }
+    }
+}
+
+
+
 
--- SimpleCalc/SimpleCalcTests/SimpleCalcTests.swift
+++ SimpleCalc/SimpleCalcTests/SimpleCalcTests.swift
@@ -11,9 +11,9 @@ import XCTest
 class ExtendedCalcTests :XCTestCase {
     let calc = Calculator()
     
-    //func testNegativeOps() {
-    //    XCTAssert(calc.calculate(["2", "+", "-12"]) == -10)
-    //}
+    func testNegativeOps() {
+        XCTAssert(calc.calculate(["2", "+", "-12"]) == -10)
+    }
 }
 
 class SimpleCalcTests: XCTestCase {
@@ -35,7 +35,7 @@ class SimpleCalcTests: XCTestCase {
     }
     
     func testAvgOps() {
-        XCTAssert(calc.calculate(["1", "2", "3", "4", "5", "avg"]) == 3) // 15 / 5 = 3
+        XCTAssert(calc.calculate(["1", "2", "3", "4", "5", "avg"]) == 3) // 15 r/ 5 = 3
         XCTAssert(calc.calculate(["2", "2", "4", "4", "avg"]) == 3) // 12 / 4 = 3
         XCTAssert(calc.calculate(["2", "avg"]) == 2) // 2 / 1 = 2
         XCTAssert(calc.calculate(["avg"]) == 0) // 0 / 0 = 0 (not really, but it's an edge case
